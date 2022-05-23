285f172e1cd1459bed36526971eab59e9fb48d41
alexmoraleja <99951166+alexmoraleja@users.noreply.github.com>
2022-05-23 13:07:01 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(2000)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
     basic.pause(2000)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
     basic.pause(2000)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 150)
     basic.pause(2000)
