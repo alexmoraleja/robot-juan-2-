6fe750f6d86740b7b2bc2ed0b53a12bfa4265d95
alexmoraleja <99951166+alexmoraleja@users.noreply.github.com>
2022-05-30 12:41:11 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 1000; index++) {
+    for (let index = 0; index < 25; index++) {
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
         music.playTone(988, music.beat(BeatFraction.Double))
         strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-        music.playTone(392, music.beat(BeatFraction.Double))
+        music.playTone(698, music.beat(BeatFraction.Double))
     }
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 1000; index++) {
+    for (let index = 0; index < 25; index++) {
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
         music.playTone(988, music.beat(BeatFraction.Half))
         strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(698, music.beat(BeatFraction.Half))
     }
 })
 let strip: neopixel.Strip = null
